7539c8ed2a589b7ed71e8247e58f62a00800bee6
zadididon <zadididon@gmail.com>
2024-07-06 15:57:00 +0200

    Salary HW4

---
+++ HW4.1/Salary.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+
+# open file and read them:
+def total_salary(path):
+
+    with open (path, "r", encoding= 'utf8') as file: 
+        lines = [el.strip() for el in file.readlines()]
+
+#total salary is now set at 0
+        total = 0
+        workers = 0
+
+
+        for line in lines:
+            name, salary_str = line.split(',') # -> list ["name", "salary"]
+            salary  = int(salary_str)
+            total += salary
+            workers += 1
+
+        if workers == 0:
+            return (0,0)
+            
+        average_salary = total / workers
+        return (total, average_salary)
+
+
+total, average = total_salary("workers.txt")      
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
