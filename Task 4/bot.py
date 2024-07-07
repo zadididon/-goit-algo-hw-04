a62c9035be3f6e4c613b1cad8cbdf00184518b5f
zadididon <zadididon@gmail.com>
2024-07-07 13:42:50 +0200

    bot

--- Task 4/bot.py
+++
@@ -1,7 +0,0 @@
-def understand_input(user_input):
-    user_input = user_input.strip().lower
-    command, *args = user_input.split()
-    return command, args
-
-
-
---
+++ Task 4/main.py
@@ -0,0 +1,18 @@
+def main():
+    print("Welcome to the assistant bot!")
+    while True:
+        command = input("Enter a command: ").strip().lower()
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
+
+
